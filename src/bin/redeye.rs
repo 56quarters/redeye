ac76a6a811b236eedda84e440b2808d34f7e207f
Nick Pillitteri <nick@tshlabs.org>
2018-07-27 08:28:17 -0400

    Start of flushing on buf size + timer

--- src/bin/redeye.rs
+++ src/bin/redeye.rs
@@ -10,7 +10,7 @@ extern crate redeye;
 extern crate tokio;
 
 use futures::sync::mpsc;
-use redeye::buf::LogBuffer;
+use redeye::buf::{LogBuffer, NeedFlush};
 use redeye::enrich::LineParser;
 use redeye::input::StdinBufReader;
 use redeye::send::BackPressureSender;
@@ -38,41 +38,52 @@ fn delayed_receiver(rx: mpsc::Receiver<String>, delay: u64) -> impl Future<Item 
         })
 }
 
+enum FlushReason {
+    Full,
+    Timer,
+}
+
 fn main() {
-    let stdin = StdinBufReader::new(io::stdin());
-    let buf = Arc::new(LogBuffer::new());
-    let buf_flush = Arc::clone(&buf);
-    let buf_send = Arc::clone(&buf);
+    let buf_send = Arc::new(LogBuffer::default());
+    let buf_flush = Arc::clone(&buf_send);
     let parser = Arc::new(LineParser::default());
 
-    let lines = io::lines(stdin)
+    let (tx_flush, rx_flush) = mpsc::channel(1);
+    let sender_flush = BackPressureSender::new(tx_flush);
+
+    let lines = io::lines(StdinBufReader::default())
         .map_err(|err| RedeyeError::from(err))
-        .for_each(move |line| {
-            let msg = parser.parse_line(line)?;
+        .and_then(move |line| parser.parse_line(line))
+        .for_each(move |msg| {
             buf_send.push(msg);
-            //buf_send.push(line);
             Ok(())
         })
         .map_err(|err| {
             eprintln!("Line error: {:?}", err);
         });
 
-    let (tx, rx) = mpsc::channel(100);
-    let sender = BackPressureSender::new(tx);
+    let (tx_backend, rx_backend) = mpsc::channel(1);
+    let sender_backend = BackPressureSender::new(tx_backend);
     let start = Instant::now() + Duration::from_secs(1);
 
     let period = Interval::new(start, Duration::from_millis(1000))
-        .map_err(|err| RedeyeError::from(err))
-        .for_each(move |_instant| sender.send(buf_flush.flush()))
+        .map(|_instant| ())
         .map_err(|err| {
             eprintln!("Period error: {:?}", err);
+        })
+        .select(rx_flush)
+        .for_each(move |_instant| {
+            sender_backend.send(buf_flush.flush()).map_err(|err| {
+                eprintln!("Flush error: {:?}", err);
+            })
         });
 
-    let backend =
-        rx.for_each(|batch| {
+    let backend = rx_backend
+        .for_each(|batch| {
             eprintln!("Received {} entries in backend", batch.len());
             Ok(())
-        }).map_err(|err| {
+        })
+        .map_err(|err| {
             eprintln!("Backend error: {:?}", err);
         });
 
--- src/redeye/buf.rs
+++ src/redeye/buf.rs
@@ -8,11 +8,21 @@ use std::fmt::Debug;
 use std::mem;
 use std::sync::Mutex;
 
+const DEFAULT_FLUSH_SIZE: usize = 10_000;
+const DEFAULT_BUFFER_SIZE: usize = 128;
+
+pub enum NeedFlush {
+    Yes,
+    No,
+}
+
 #[derive(Debug)]
 pub struct LogBuffer<T>
 where
     T: Debug,
 {
+    flush_sz: usize,
+    buffer_sz: usize,
     buf: Mutex<RefCell<Vec<T>>>,
 }
 
@@ -20,21 +30,42 @@ impl<T> LogBuffer<T>
 where
     T: Debug,
 {
-    pub fn new() -> Self {
+    pub fn new(flush_sz: usize) -> Self {
+        Self::with_buf_size(flush_sz, DEFAULT_BUFFER_SIZE)
+    }
+
+    pub fn with_buf_size(flush_sz: usize, buffer_sz: usize) -> Self {
         LogBuffer {
-            buf: Mutex::new(RefCell::new(Vec::new())),
+            flush_sz,
+            buffer_sz,
+            buf: Mutex::new(RefCell::new(Vec::with_capacity(buffer_sz))),
         }
     }
 
-    pub fn push(&self, line: T) {
+    pub fn push(&self, val: T) -> NeedFlush {
         let cell = self.buf.lock().unwrap();
         let mut buf = cell.borrow_mut();
-        buf.push(line);
+        buf.push(val);
+
+        if buf.len() >= self.flush_sz {
+            NeedFlush::Yes
+        } else {
+            NeedFlush::No
+        }
     }
 
     pub fn flush(&self) -> Vec<T> {
         let cell = self.buf.lock().unwrap();
         let mut buf = cell.borrow_mut();
-        mem::replace(&mut buf, Vec::new())
+        mem::replace(&mut buf, Vec::with_capacity(self.buffer_sz))
+    }
+}
+
+impl<T> Default for LogBuffer<T>
+where
+    T: Debug,
+{
+    fn default() -> Self {
+        Self::new(DEFAULT_FLUSH_SIZE)
     }
 }
