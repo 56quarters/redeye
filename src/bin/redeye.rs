43462c9de617ced13debac25d1004e2d8ed9ef2a
Nick Pillitteri <nick@tshlabs.org>
2018-08-12 19:48:59 -0400

    Clap + task cleanup

--- src/bin/redeye.rs
+++ src/bin/redeye.rs
@@ -5,6 +5,7 @@
 
 //!
 
+extern crate clap;
 extern crate futures;
 extern crate redeye;
 extern crate serde_json;
@@ -14,20 +15,21 @@ use redeye::input::StdinBufReader;
 use redeye::parser::{CommonLogLineParser, LogLineParser};
 use redeye::types::RedeyeError;
 use std::io::BufRead;
-use tokio::io::lines;
+use tokio::io::{lines, stdout};
 use tokio::prelude::*;
 
-fn new_stdin_task<R, P>(reader: R, parser: P) -> impl Future<Item = (), Error = ()>
+fn new_parser_task<R, P, W>(reader: R, parser: P, mut writer: W) -> impl Future<Item = (), Error = ()>
 where
     R: AsyncRead + BufRead,
     P: LogLineParser,
+    W: AsyncWrite,
 {
     lines(reader)
-        .map_err(|e| RedeyeError::from(e))
+        .map_err(RedeyeError::from)
         .and_then(move |line| parser.parse(&line))
-        .and_then(|event| serde_json::to_string(&event).map_err(|e| RedeyeError::from(e)))
-        .for_each(|json| {
-            println!("{}", json);
+        .and_then(|event| serde_json::to_string(&event).map_err(RedeyeError::from))
+        .for_each(move |json| {
+            writeln!(writer, "{}", json);
             Ok(())
         }).map_err(|e| {
             handle_redeye_error(e);
@@ -39,8 +41,10 @@ fn handle_redeye_error(err: RedeyeError) {
 }
 
 fn main() {
+    let reader = StdinBufReader::default();
     let parser = CommonLogLineParser::new();
-    let lines = new_stdin_task(StdinBufReader::default(), parser);
+    let writer = stdout();
+    let lines = new_parser_task(reader, parser, writer);
 
     tokio::run(lines);
 }
