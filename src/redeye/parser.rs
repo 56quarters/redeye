aad9de5493c38fc28723301ca7f0b67ef8e985e2
Nick Pillitteri <nick@tshlabs.org>
2018-08-16 22:42:00 -0400

    Move some common logic to an inner parser

--- src/redeye/parser.rs
+++ src/redeye/parser.rs
@@ -14,13 +14,13 @@ pub trait LogLineParser {
 }
 
 pub struct CommonLogLineParser {
-    regex: Regex,
+    inner: InnerParser,
 }
 
 impl CommonLogLineParser {
     pub fn new() -> Self {
         Self {
-            regex: Regex::new(concat!(
+            inner: InnerParser::new(Regex::new(concat!(
                 r"^([^\s]+)\s+", // host
                 r"([^\s]+)\s+",  // rfc931 ident
                 r"([^\s]+)\s+",  // username
@@ -32,72 +32,72 @@ impl CommonLogLineParser {
                 ")\"\\s+",       // close " and HTTP request
                 r"([^\s]+)\s+",  // status
                 r"([^\s]+)$",    // bytes
-            )).unwrap(),
+            )).unwrap())
         }
     }
 }
 
 impl LogLineParser for CommonLogLineParser {
     fn parse(&self, line: &str) -> RedeyeResult<LogEvent> {
-        line_to_event(&self.regex, line, |matches, full_line, mut map| {
-            add_field(&mut map, "remote_host", parse_text_value(&matches, 1, full_line))?;
-            add_field(&mut map, "ident", parse_text_value(&matches, 2, full_line))?;
-            add_field(&mut map, "username", parse_text_value(&matches, 3, full_line))?;
-            add_field(&mut map, "@timestamp", parse_timestamp(&matches, 4, full_line, COMMON_LOG_TIMESTAMP))?;
-            add_field(&mut map, "requested_url", parse_text_value(&matches, 5, full_line))?;
-            add_field(&mut map, "method", parse_text_value(&matches, 6, full_line))?;
-            add_field(&mut map, "requested_uri", parse_text_value(&matches, 7, full_line))?;
-            add_field(&mut map, "protocol", parse_text_value(&matches, 8, full_line))?;
-            add_field(&mut map, "status_code", parse_int_value(&matches, 9, full_line))?;
-            add_field(&mut map, "content_length", parse_int_value(&matches, 10, full_line))?;
+        self.inner.apply(line, |matches, full_line, mut map| {
+            add_field(&mut map, "remote_host", parse_text_value(matches, 1, full_line))?;
+            add_field(&mut map, "ident", parse_text_value(matches, 2, full_line))?;
+            add_field(&mut map, "username", parse_text_value(matches, 3, full_line))?;
+            add_field(&mut map, "@timestamp", parse_timestamp(matches, 4, full_line, COMMON_LOG_TIMESTAMP))?;
+            add_field(&mut map, "requested_url", parse_text_value(matches, 5, full_line))?;
+            add_field(&mut map, "method", parse_text_value(matches, 6, full_line))?;
+            add_field(&mut map, "requested_uri", parse_text_value(matches, 7, full_line))?;
+            add_field(&mut map, "protocol", parse_text_value(matches, 8, full_line))?;
+            add_field(&mut map, "status_code", parse_int_value(matches, 9, full_line))?;
+            add_field(&mut map, "content_length", parse_int_value(matches, 10, full_line))?;
             Ok(())
         })
     }
 }
 
 pub struct CombinedLogLineParser {
-    regex: Regex,
+    inner: InnerParser,
 }
 
 impl CombinedLogLineParser {
     pub fn new() -> Self {
         Self {
-            regex: Regex::new(concat!(
-                r"^([^\s]+)\s+",     // host
-                r"([^\s]+)\s+",      // rfc931 ident
-                r"([^\s]+)\s+",      // username
-                r"\[(.+)\]\s+",      // timestamp
-                "\"(",               // open " and HTTP request
-                r"([^\s]+)\s",       // method
-                r"([^\s]+)\s",       // path
-                r"([^\s]+)",         // protocol
-                ")\"\\s+",           // close " and HTTP request
-                r"([^\s]+)\s+",      // status
-                r"([^\s]+)\s+",      // bytes
-                "\"([^\"]+)\"\\s+",  // "referer" [sic]
-                "\"([^\"]+)\"$",     // "user agent"
-            )).unwrap(),
+            inner: InnerParser::new(Regex::new(concat!(
+                r"^([^\s]+)\s+",    // host
+                r"([^\s]+)\s+",     // rfc931 ident
+                r"([^\s]+)\s+",     // username
+                r"\[(.+)\]\s+",     // timestamp
+                "\"(",              // open " and HTTP request
+                r"([^\s]+)\s",      // method
+                r"([^\s]+)\s",      // path
+                r"([^\s]+)",        // protocol
+                ")\"\\s+",          // close " and HTTP request
+                r"([^\s]+)\s+",     // status
+                r"([^\s]+)\s+",     // bytes
+                "\"([^\"]+)\"\\s+", // "referer" [sic]
+                "\"([^\"]+)\"$",    // "user agent"
+            )).unwrap())
         }
     }
 }
 
 impl LogLineParser for CombinedLogLineParser {
     fn parse(&self, line: &str) -> RedeyeResult<LogEvent> {
-        line_to_event(&self.regex, line, |matches, full_line, mut map| {
-            add_field(&mut map, "remote_host", parse_text_value(&matches, 1, full_line))?;
-            add_field(&mut map, "ident", parse_text_value(&matches, 2, full_line))?;
-            add_field(&mut map, "username", parse_text_value(&matches, 3, full_line))?;
-            add_field(&mut map, "@timestamp", parse_timestamp(&matches, 4, full_line, COMMON_LOG_TIMESTAMP))?;
-            add_field(&mut map, "requested_url", parse_text_value(&matches, 5, full_line))?;
-            add_field(&mut map, "method", parse_text_value(&matches, 6, full_line))?;
-            add_field(&mut map, "requested_uri", parse_text_value(&matches, 7, full_line))?;
-            add_field(&mut map, "protocol", parse_text_value(&matches, 8, full_line))?;
-            add_field(&mut map, "status_code", parse_int_value(&matches, 9, full_line))?;
-            add_field(&mut map, "content_length", parse_int_value(&matches, 10, full_line))?;
+        self.inner.apply(line, |matches, full_line, mut map| {
+            add_field(&mut map, "remote_host", parse_text_value(matches, 1, full_line))?;
+            add_field(&mut map, "ident", parse_text_value(matches, 2, full_line))?;
+            add_field(&mut map, "username", parse_text_value(matches, 3, full_line))?;
+            add_field(&mut map, "@timestamp", parse_timestamp(matches, 4, full_line, COMMON_LOG_TIMESTAMP))?;
+            add_field(&mut map, "requested_url", parse_text_value(matches, 5, full_line))?;
+            add_field(&mut map, "method", parse_text_value(matches, 6, full_line))?;
+            add_field(&mut map, "requested_uri", parse_text_value(matches, 7, full_line))?;
+            add_field(&mut map, "protocol", parse_text_value(matches, 8, full_line))?;
+            add_field(&mut map, "status_code", parse_int_value(matches, 9, full_line))?;
+            add_field(&mut map, "content_length", parse_int_value(matches, 10, full_line))?;
 
             let mut headers = HashMap::with_capacity(2);
-            add_field(&mut headers, "referer" /* [sic] */, parse_text_value(&matches, 11, full_line))?;
-            add_field(&mut headers, "user-agent", parse_text_value(&matches, 12, full_line))?;
+            add_field(&mut headers, "referer" /* [sic] */, parse_text_value(matches, 11, full_line))?;
+            add_field(&mut headers, "user-agent", parse_text_value(matches, 12, full_line))?;
 
             if !headers.is_empty() {
                 map.insert("request_headers".to_string(), LogFieldValue::Mapping(headers));
@@ -108,18 +108,33 @@ impl LogLineParser for CombinedLogLineParser {
     }
 }
 
-fn line_to_event<F>(reg: &Regex, line: &str, func: F) -> RedeyeResult<LogEvent>
-where
-    F: FnOnce(&Captures, &str, &mut HashMap<String, LogFieldValue>) -> RedeyeResult<()>
-{
-    reg
-        .captures(line.trim())
-        .ok_or_else(|| RedeyeError::ParseError(line.to_string()))
-        .and_then(|matches| {
-            let mut map = HashMap::with_capacity(matches.len());
-            func(&matches, line, &mut map)?;
-            Ok(LogEvent::from(map))
-        })
+struct InnerParser {
+    regex: Regex,
+}
+
+impl InnerParser {
+    fn new(regex: Regex) -> Self {
+        Self { regex }
+    }
+
+    fn apply<F>(&self, line: &str, func: F) -> RedeyeResult<LogEvent>
+        where
+            F: FnOnce(&Captures, &str, &mut HashMap<String, LogFieldValue>) -> RedeyeResult<()>,
+    {
+        self.regex
+            .captures(line.trim())
+            .ok_or_else(|| RedeyeError::ParseError(line.to_string()))
+            .and_then(|matches| {
+                let mut map = HashMap::with_capacity(matches.len());
+                func(&matches, line, &mut map)?;
+                Ok(LogEvent::from(map))
+            })
+    }
+}
+
+struct InnerParseOutput<'a> {
+    captures: &'a Captures<'a>,
+    output: &'a mut HashMap<String, LogFieldValue>,
 }
 
 fn add_field<S>(
