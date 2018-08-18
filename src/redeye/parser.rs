5d96972762e03cc043e9fb8aecbd726a2206e97b
Nick Pillitteri <nick@tshlabs.org>
2018-08-18 14:13:54 -0400

    Recursive parser

--- src/redeye/parser.rs
+++ src/redeye/parser.rs
@@ -5,6 +5,7 @@
 use chrono::DateTime;
 use regex::{Captures, Regex};
 use std::collections::HashMap;
+use std::rc::Rc;
 use types::{LogEvent, LogFieldValue, RedeyeError, RedeyeResult};
 
 const COMMON_LOG_TIMESTAMP: &str = "%d/%b/%Y:%T %z";
@@ -13,6 +14,7 @@ pub trait LogLineParser {
     fn parse(&self, line: &str) -> RedeyeResult<LogEvent>;
 }
 
+#[derive(Debug, Clone)]
 pub struct CommonLogLineParser {
     inner: InnerParser,
 }
@@ -20,26 +22,30 @@ pub struct CommonLogLineParser {
 impl CommonLogLineParser {
     pub fn new() -> Self {
         Self {
-            inner: InnerParser::new(Regex::new(concat!(
-                r"^([^\s]+)\s+", // host
-                r"([^\s]+)\s+",  // rfc931 ident
-                r"([^\s]+)\s+",  // username
-                r"\[(.+)\]\s+",  // timestamp
-                "\"(",           // open " and HTTP request
-                r"([^\s]+)\s",   // method
-                r"([^\s]+)\s",   // path
-                r"([^\s]+)",     // protocol
-                ")\"\\s+",       // close " and HTTP request
-                r"([^\s]+)\s+",  // status
-                r"([^\s]+)$",    // bytes
-            )).unwrap())
+            inner: InnerParser::new(
+                Regex::new(concat!(
+                    r"^([^\s]+)\s+", // host
+                    r"([^\s]+)\s+",  // rfc931 ident
+                    r"([^\s]+)\s+",  // username
+                    r"\[(.+)\]\s+",  // timestamp
+                    "\"(",           // open " and HTTP request
+                    r"([^\s]+)\s",   // method
+                    r"([^\s]+)\s",   // path
+                    r"([^\s]+)",     // protocol
+                    ")\"\\s+",       // close " and HTTP request
+                    r"([^\s]+)\s+",  // status
+                    r"([^\s]+)$",    // bytes
+                )).unwrap(),
+            ),
         }
     }
 }
 
 impl LogLineParser for CommonLogLineParser {
     fn parse(&self, line: &str) -> RedeyeResult<LogEvent> {
-        let fields = self.inner.apply(line)?
+        let fields = self
+            .inner
+            .apply(line)?
             .add_text_field("remote_host", 1)?
             .add_text_field("ident", 2)?
             .add_text_field("username", 3)?
@@ -56,6 +62,7 @@ impl LogLineParser for CommonLogLineParser {
     }
 }
 
+#[derive(Debug, Clone)]
 pub struct CombinedLogLineParser {
     inner: InnerParser,
 }
@@ -63,29 +70,32 @@ pub struct CombinedLogLineParser {
 impl CombinedLogLineParser {
     pub fn new() -> Self {
         Self {
-            inner: InnerParser::new(Regex::new(concat!(
-                r"^([^\s]+)\s+",    // host
-                r"([^\s]+)\s+",     // rfc931 ident
-                r"([^\s]+)\s+",     // username
-                r"\[(.+)\]\s+",     // timestamp
-                "\"(",              // open " and HTTP request
-                r"([^\s]+)\s",      // method
-                r"([^\s]+)\s",      // path
-                r"([^\s]+)",        // protocol
-                ")\"\\s+",          // close " and HTTP request
-                r"([^\s]+)\s+",     // status
-                r"([^\s]+)\s+",     // bytes
-                "\"([^\"]+)\"\\s+", // "referer" [sic]
-                "\"([^\"]+)\"$",    // "user agent"
-            )).unwrap())
+            inner: InnerParser::new(
+                Regex::new(concat!(
+                    r"^([^\s]+)\s+",    // host
+                    r"([^\s]+)\s+",     // rfc931 ident
+                    r"([^\s]+)\s+",     // username
+                    r"\[(.+)\]\s+",     // timestamp
+                    "\"(",              // open " and HTTP request
+                    r"([^\s]+)\s",      // method
+                    r"([^\s]+)\s",      // path
+                    r"([^\s]+)",        // protocol
+                    ")\"\\s+",          // close " and HTTP request
+                    r"([^\s]+)\s+",     // status
+                    r"([^\s]+)\s+",     // bytes
+                    "\"([^\"]+)\"\\s+", // "referer" [sic]
+                    "\"([^\"]+)\"$",    // "user agent"
+                )).unwrap(),
+            ),
         }
     }
 }
 
 impl LogLineParser for CombinedLogLineParser {
     fn parse(&self, line: &str) -> RedeyeResult<LogEvent> {
-
-        let fields = self.inner.apply(line)?
+        let fields = self
+            .inner
+            .apply(line)?
             .add_text_field("remote_host", 1)?
             .add_text_field("ident", 2)?
             .add_text_field("username", 3)?
@@ -98,14 +108,15 @@ impl LogLineParser for CombinedLogLineParser {
             .add_int_field("content_length", 10)?
             .add_mapping_field("request_headers")
             .add_text_field("referer", 11)?
-            .add_text_field("user-agent", 12)?
-            .build_mapping()
+            .add_text_field("user_agent", 12)?
+            .complete_mapping()
             .build();
 
         Ok(LogEvent::from(fields))
     }
 }
 
+#[derive(Debug, Clone)]
 struct InnerParser {
     regex: Regex,
 }
@@ -115,85 +126,57 @@ impl InnerParser {
         Self { regex }
     }
 
-    fn apply<'a, 'b>(&'a self, line: &'b str) -> RedeyeResult<InnerParseOutput>
+    fn apply<'a, 'b>(&'a self, line: &'b str) -> RedeyeResult<ParserState>
     where
-        'b: 'a
+        'b: 'a,
     {
         self.regex
             .captures(line.trim())
             .ok_or_else(|| RedeyeError::ParseError(line.to_string()))
-            .map(|matches| {
-                InnerParseOutput::from(line, matches)
-            })
+            .map(|matches| ParserState::root(line, matches))
     }
 }
 
-struct MappingBuilder<'a> {
-    field: String,
-    values: HashMap<String, LogFieldValue>,
-    output: InnerParseOutput<'a>,
-}
-
-impl<'a> MappingBuilder<'a> {
-    fn add_text_field<S>(mut self, field: S, index: usize) -> RedeyeResult<Self>
-        where S: Into<String>
-    {
-        let res = parse_text_value(&self.output.captures, index, self.output.line)?;
-        if let Some(v) = res {
-            self.values.insert(field.into(), v);
-        }
-
-        Ok(self)
-
-    }
-    fn add_timestamp_field<S>(mut self, field: S, index: usize, format: &str) -> RedeyeResult<Self>
-        where S: Into<String>
-    {
-        let res = parse_timestamp(&self.output.captures, index, self.output.line, format)?;
-        if let Some(v) = res {
-            self.values.insert(field.into(), v);
-        }
-
-        Ok(self)
-    }
-    fn add_int_field<S>(mut self, field: S, index: usize) -> RedeyeResult<Self>
-        where S: Into<String>
-    {
-        let res = parse_int_value(&self.output.captures, index, self.output.line)?;
-        if let Some(v) = res {
-            self.values.insert(field.into(), v);
-        }
-
-        Ok(self)
-    }
-
-    fn build_mapping(mut self) -> InnerParseOutput<'a> {
-        if !self.values.is_empty() {
-            self.output.values.insert(self.field, LogFieldValue::Mapping(self.values));
-        }
-
-        self.output
-    }
-}
-
-struct InnerParseOutput<'a> {
-    values: HashMap<String, LogFieldValue>,
+#[derive(Debug)]
+struct ParserState<'a> {
     line: &'a str,
-    captures: Captures<'a>,
+    captures: Rc<Captures<'a>>,
+    field: Option<String>,
+    parent: Option<Box<ParserState<'a>>>,
+    values: HashMap<String, LogFieldValue>,
 }
 
-impl<'a> InnerParseOutput<'a> {
-    fn from(line: &'a str, captures: Captures<'a>) -> Self {
+impl<'a> ParserState<'a> {
+    fn root(line: &'a str, captures: Captures<'a>) -> Self {
         let len = captures.len();
-        Self {
-            captures,
+
+        ParserState {
             line,
+            captures: Rc::new(captures),
+            field: None,
+            parent: None,
             values: HashMap::with_capacity(len),
         }
     }
 
+    fn leaf(
+        line: &'a str,
+        captures: Rc<Captures<'a>>,
+        field: String,
+        parent: Box<ParserState<'a>>,
+    ) -> Self {
+        ParserState {
+            line,
+            captures,
+            field: Some(field),
+            parent: Some(parent),
+            values: HashMap::new(),
+        }
+    }
+
     fn add_text_field<S>(mut self, field: S, index: usize) -> RedeyeResult<Self>
-        where S: Into<String>
+    where
+        S: Into<String>,
     {
         let res = parse_text_value(&self.captures, index, self.line)?;
         if let Some(v) = res {
@@ -201,10 +184,10 @@ impl<'a> InnerParseOutput<'a> {
         }
 
         Ok(self)
-
     }
     fn add_timestamp_field<S>(mut self, field: S, index: usize, format: &str) -> RedeyeResult<Self>
-        where S: Into<String>
+    where
+        S: Into<String>,
     {
         let res = parse_timestamp(&self.captures, index, self.line, format)?;
         if let Some(v) = res {
@@ -214,7 +197,8 @@ impl<'a> InnerParseOutput<'a> {
         Ok(self)
     }
     fn add_int_field<S>(mut self, field: S, index: usize) -> RedeyeResult<Self>
-        where S: Into<String>
+    where
+        S: Into<String>,
     {
         let res = parse_int_value(&self.captures, index, self.line)?;
         if let Some(v) = res {
@@ -224,22 +208,32 @@ impl<'a> InnerParseOutput<'a> {
         Ok(self)
     }
 
-    fn add_mapping_field<S>(self, field: S) -> MappingBuilder<'a>
+    fn add_mapping_field<S>(self, field: S) -> Self
     where
-    S: Into<String>
+        S: Into<String>,
     {
-        MappingBuilder {
-            field: field.into(),
-            values: HashMap::new(),
-            output: self,
+        let parent = Box::new(self);
+        ParserState::leaf(parent.line, parent.captures.clone(), field.into(), parent)
+    }
+
+    fn complete_mapping(self) -> Box<Self> {
+        // Unwraps are OK here because if we're calling this method when not building
+        // a nested mapping, that's a bug completely within our control and panicking
+        // is the most obvious way to handle it.
+        let mut parent = self.parent.unwrap();
+        if !self.values.is_empty() {
+            parent
+                .values
+                .insert(self.field.unwrap(), LogFieldValue::Mapping(self.values));
         }
+
+        parent
     }
 
     fn build(self) -> HashMap<String, LogFieldValue> {
         self.values
     }
 }
-
 
 fn parse_timestamp(
     matches: &Captures,
