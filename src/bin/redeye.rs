0b77eb29ae9e6d95221250eb0a1df5f85488d39a
Nick Pillitteri <nick@tshlabs.org>
2018-08-20 22:19:24 -0400

    Add license

--- src/bin/redeye.rs
+++ src/bin/redeye.rs
@@ -1,6 +1,19 @@
+// Redeye - Parse Apache-style access logs into Logstash JSON
 //
+// Copyright 2018 TSH Labs
 //
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 //!
@@ -28,7 +41,7 @@ fn parse_cli_opts<'a>(args: Vec<String>) -> ArgMatches<'a> {
         .version(crate_version!())
         .set_term_width(MAX_TERM_WIDTH)
         .about(
-            "\nRedeye converts Apache httpd style access to JSON understood \
+            "\nRedeye converts NCSA or Apache HTTPD style access to JSON understood \
              by Logstash. Access log entries are read line by line from stdin, \
              converted to Logstash JSON, and emitted on stdout. Currently \
              Common and Combined access log formats are supported. For more \
