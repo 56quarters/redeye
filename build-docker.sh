74d16adff42233bf34c03116a10673262bc09a60
Nick Pillitteri <nick@tshlabs.org>
2018-09-08 22:01:20 -0400

    Build docker image from tags

--- build-docker.sh
+++ build-docker.sh
@@ -4,11 +4,11 @@ set -o xtrace
 set -o errexit
 
 PROJECT="tshlabs/redeye"
-#VERSION=`git describe --abbrev=0 --tags`
+VERSION=`git describe --abbrev=0 --tags`
 
-#git checkout "$VERSION"
+git checkout "$VERSION"
 docker build -t "${PROJECT}:latest" .
-#docker tag "${PROJECT}:latest" "${PROJECT}:${VERSION}"
-#git checkout -
+docker tag "${PROJECT}:latest" "${PROJECT}:${VERSION}"
+git checkout -
 
 docker run -ti --rm "${PROJECT}:latest"
