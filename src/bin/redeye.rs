62cf40ddc60db304daac67357b526a7871ac7d3c
Nick Pillitteri <nick@tshlabs.org>
2020-06-17 20:18:35 -0400

    Copyright update

--- src/bin/redeye.rs
+++ src/bin/redeye.rs
@@ -1,6 +1,6 @@
 // Redeye - Parse Apache-style access logs into Logstash JSON
 //
-// Copyright 2018 TSH Labs
+// Copyright 2018 Nick Pillitteri
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
